568d5cd00f56bb4ed965277850784f1b37a6d803
Abdullah Al Afique <afique.dev@gmail.com>
2025-02-15 14:02:00 +0600

    Implement radar system for environmental sensing

--- support/newcar.py
+++ support/newcar.py
@@ -1,26 +1,57 @@
+import math
+import random
+import sys
+import os
+
 import neat
 import pygame
-import math
+
+# Constants
+# WIDTH = 1600
+# HEIGHT = 880
 
 WIDTH = 1920
 HEIGHT = 1080
-CAR_SIZE_X = 60
+
+CAR_SIZE_X = 60    
 CAR_SIZE_Y = 60
-BORDER_COLOR = (255, 255, 255, 255)
+
+BORDER_COLOR = (255, 255, 255, 255) # Color To Crash on Hit
+
+current_generation = 0 # Generation counter
 
 class Car:
+
     def __init__(self):
-        self.sprite = pygame.image.load('car.png').convert()
+        # Load Car Sprite and Rotate
+        self.sprite = pygame.image.load('car.png').convert() # Convert Speeds Up A Lot
         self.sprite = pygame.transform.scale(self.sprite, (CAR_SIZE_X, CAR_SIZE_Y))
-        self.rotated_sprite = self.sprite
-        self.position = [830, 920]
+        self.rotated_sprite = self.sprite 
+
+        # self.position = [690, 740] # Starting Position
+        self.position = [830, 920] # Starting Position
         self.angle = 0
         self.speed = 0
-        self.center = [self.position[0] + CAR_SIZE_X / 2, self.position[1] + CAR_SIZE_Y / 2]
-        self.alive = True
 
+        self.speed_set = False # Flag For Default Speed Later on
 
-    def update(self, game_map):
-        self.rotated_sprite = self.rotate_center(self.sprite, self.angle)
-        self.position[0] += math.cos(math.radians(360 - self.angle)) * self.speed
-        self.position[1] += math.sin(math.radians(360 - self.angle)) * self.speed
+        self.center = [self.position[0] + CAR_SIZE_X / 2, self.position[1] + CAR_SIZE_Y / 2] # Calculate Center
+
+        self.radars = [] # List For Sensors / Radars
+        self.drawing_radars = [] # Radars To Be Drawn
+
+        self.alive = True # Boolean To Check If Car is Crashed
+
+        self.distance = 0 # Distance Driven
+        self.time = 0 # Time Passed
+
+    def draw(self, screen):
+        screen.blit(self.rotated_sprite, self.position) # Draw Sprite
+        self.draw_radar(screen) #OPTIONAL FOR SENSORS
+
+    def draw_radar(self, screen):
+        # Optionally Draw All Sensors / Radars
+        for radar in self.radars:
+            position = radar[0]
+            pygame.draw.line(screen, (0, 255, 0), self.center, position, 1)
+            pygame.draw.circle(screen, (0, 255, 0), position, 5)
